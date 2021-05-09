cc37ec0c0caf69d4443789f0b33f7f2cbd2abbc9
KamrunNaher0175 <80691119+KamrunNaher0175@users.noreply.github.com>
2021-05-09 18:34:23 +0600

    Add files via upload

--- Emergency Service/models.py
+++ Emergency Service/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class detail(models.Model):
+
+    def __str__(self):
+        return self.name
+    
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=300)
+    phone = models.IntegerField()
+    status = models.CharField(max_length=50, default="register")
--- Emergency Service/views.py
+++ Emergency Service/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from .models import detail
+from django.views.generic.list import ListView
 # Create your views here.
-#class based views
-def index(request):
-    return render(request, 'hospital/index.html')
+
+
+
+
+@login_required
+
+def detail_view(request):
+    detail_view = detail.objects.all()
+    context = {
+        'detail_view' : detail_view
+    }
+    return render(request, 'registration/detail.html', context)
